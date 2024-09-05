e0c1004cbfec395c035e8fb2d75602955660f280
meetmetha <miteshmehta2018@gmail.com>
2024-09-05 21:20:55 +0530

    Working Inference pushed

--- exec/inference.py
+++ exec/inference.py
@@ -3,7 +3,17 @@ import json
 import logging
 def fetchModelResponse(resData):
     model_name = resData['model_name']
-    response = requests.post(f"http://localhost:8080/predictions/{model_name}", data={'data':json.dumps(resData)})
-    logging.info("Model Response",response.json())
-    #return {"accuracy":99,"isPositive":True}
+    events = [
+        {'event_id':resData['event_id'], 'game_id':resData['game_id'],'url':resData['url']}
+    ]
+    response = requests.post(f"http://localhost:8080/predictions/{model_name}", data={'data':json.dumps(events)})
+    print(response.json())
+    logging.info("Model Response",response)
     return response.json()
+
+'''fetchModelResponse({
+    "game_id":"1",
+    "event_id":"1",
+    "url": "s3://playai-cv-video-filter-prod/data/sample_data/gamecls=MINECRAFT/game_id=1/event_id=1.npy",
+    "model_name":"pubg_mvit_v2/2.0"
+})'''
